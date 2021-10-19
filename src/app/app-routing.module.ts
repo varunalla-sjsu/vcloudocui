2f8b6130bf8dc8a26bf15f16c8fc8b092be8a9e9
Varun Alla <varun.alla@sjsu.edu>
2021-10-18 21:54:07 -0700

    guard added to protect routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import {AdminComponent} from './components/admin/admin.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { UploadfileComponent } from './components/uploadfile/uploadfile.component';
-const routes: Routes = [{path:'',redirectTo:'/dashboard', pathMatch:'full'},{path:'dashboard',component:DashboardComponent},{path:'uploadfile',component:UploadfileComponent}];
+const routes: Routes = [
+  {path:'',redirectTo:'/dashboard', pathMatch:'full'},
+  {path:'dashboard',component:DashboardComponent},
+  {path:'uploadfile',component:UploadfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
